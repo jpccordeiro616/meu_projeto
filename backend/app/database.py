114b3d04c7473db749f78aaf70f63b3f58a8b888
João Pedro <jpccordeiro616@gmail.com>
2026-03-31 17:49:01 -0300

    segundo

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://cordeiro@localhost/meu_projeto"
+DATABASE_URL = "postgresql+psycopg://postgres:Digisat123@localhost:5432/meu_projeto"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
--- backend/app/main.py
+++ backend/app/main.py
@@ -4,3 +4,8 @@ from app.routes import router
 app = FastAPI()
 
 app.include_router(router)
+
+from app.database import engine
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
