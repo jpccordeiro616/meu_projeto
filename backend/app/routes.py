98eb8e37dbfea622246043173f0acb7868aa5aeb
João Pedro <jpccordeiro616@gmail.com>
2026-04-01 08:58:12 -0300

    terceira (rotas e create user)

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,19 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
 @router.get("/")
 def home():
     return {"mensagem": "API funcionando"}
+
+
+class Usuario(BaseModel):
+    nome: str
+    email: str
+
+@router.post("/usuarios")
+def criar_usuario(usuario: Usuario):
+    return {
+        "mensagem": f"Usuário {usuario.nome} criado com sucesso"
+    }
--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,21 @@
-const botao = document.getElementById("botao")
+const botao = document.getElementById("salvar")
 const resultado = document.getElementById("resultado")
 
 botao.addEventListener("click", async () => {
-    const resposta = await fetch("http://127.0.0.1:8000/")
+    const nome = document.getElementById("nome").value
+    const email = document.getElementById("email").value
+
+    const resposta = await fetch("http://127.0.0.1:8000/usuarios", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+
     const dados = await resposta.json()
 
     resultado.innerText = dados.mensagem
